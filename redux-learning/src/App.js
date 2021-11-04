530f5695e2c4f0262282b5349b55bf7a383b7f08
bloodredhood <3drusher@gmail.com>
2021-11-04 19:00:52 +0300

    simple cash reducer

--- redux-learning/src/App.js
+++ redux-learning/src/App.js
@@ -2,15 +2,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
-  const dispatch = useDispatch
+  const dispatch = useDispatch()
   const cash = useSelector(state => state.cash)
   console.log(cash)
 
+  const addCash = (cash) => {
+    dispatch({type:"ADD_CASH", payload: cash})
+  }
+
+  const getCash = (cash) => {
+    dispatch({type:"GET_CASH", payload: cash})
+  }
+
   return (
     <div className={'App'}>
+      <div style={{fontSize: "3rem"}}>{cash}</div>
       <div style={{display: "flex"}}>
-        <button>Пополнить счет</button>
-        <button>Снять со счета</button>
+        <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+        <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
       </div>
     </div>
   )
