424f97fd8daed15197a3f54af0dbaca896ddbeba
bloodredhood <3drusher@gmail.com>
2021-11-05 18:46:05 +0300

    filling customerReducer, refactor App with action creators

--- redux-learning/src/App.js
+++ redux-learning/src/App.js
@@ -1,26 +1,56 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 function App() {
   const dispatch = useDispatch()
-  const cash = useSelector(state => state.cash)
-  console.log(cash)
+  const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
 
   const addCash = (cash) => {
-    dispatch({type:"ADD_CASH", payload: cash})
+    dispatch({ type: "ADD_CASH", payload: cash })
   }
 
   const getCash = (cash) => {
-    dispatch({type:"GET_CASH", payload: cash})
+    dispatch({ type: "GET_CASH", payload: cash })
+  }
+
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
   }
 
   return (
     <div className={'App'}>
-      <div style={{fontSize: "3rem"}}>{cash}</div>
-      <div style={{display: "flex"}}>
+      <div style={{ fontSize: "3rem" }}>{cash}</div>
+      <div style={{ display: "flex" }}>
         <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
         <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+        <button onClick={() => addCustomer((prompt()))}>Добавить клиента</button>
       </div>
+      {customers.length > 0 ?
+        <div>
+          {customers.map(customer =>
+            <div onClick={() => removeCustomer(customer)} style={{fontSize: "2rem",
+                        border: "1px solid black",
+                        paddingTop: "10px",
+                        marginTop: "10px"}}>
+              {customer.name}
+            </div>
+          )}
+        </div>
+        :
+        <div style={{fontSize: "2rem"}}>
+          Клиенты отсутствуют
+        </div>
+      }
     </div>
   )
 }
