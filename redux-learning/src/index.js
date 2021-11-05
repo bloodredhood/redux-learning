9764420de4d54d1dcfe013fbabf5e425fc4f5f49
bloodredhood <3drusher@gmail.com>
2021-11-05 17:59:37 +0300

    adding customerReducer, refactor store

--- redux-learning/src/index.js
+++ redux-learning/src/index.js
@@ -3,23 +3,8 @@ import {render} from 'react-dom';
 import App from './App';
 import {createStore} from "redux"
 import {Provider} from "react-redux"
+import { store } from "./store/store"
 
-const defaultState = {
-  cash: 0
-}
-
-const reducer = (state = defaultState, action) => {
-  switch (action.type) {
-    case "ADD_CASH":
-      return {...state, cash: state.cash + action.payload}
-    case "GET_CASH":
-      return {...state, cash: state.cash - action.payload}
-    default:
-      return state
-  }
-}
-
-const store = createStore(reducer)
 
 render(
   <Provider store={store}>
